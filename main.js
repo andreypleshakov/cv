bb7a8c9e83c5e8fb1b1b775e098a7f1e07928a2a
Andrey <andrey.pleshakov.n@gmail.com>
2025-06-05 14:05:09 +0400

    Initial clean commit

---
+++ main.js
@@ -0,0 +1,49 @@
+function showCopyNotification() {
+    const element = document.getElementById('email-copy-notification')
+
+    element.classList.remove('invisible')
+    setTimeout(() => element.classList.add('invisible'), 50)
+}
+
+function showDownloadOptions() {
+    document.getElementById('download-button').style.transition = ''
+    document.getElementById('download-options').style.display = 'block'
+    document.getElementById('download-button').style.borderRadius = '7px 7px 0 0'
+}
+
+function closeDownloadOptions() {
+    document.getElementById('download-options').style.display = 'none'
+    document.getElementById('download-button').style.borderRadius = '7px'
+}
+
+window.copyEmail = async () => {
+    const email = document.getElementById('email').innerText
+    await navigator.clipboard.writeText(email)
+    showCopyNotification()
+}
+
+window.showDownloadOptions = () => {
+    const element = document.getElementById('download-options')
+    if (element.style.display === 'block') {
+        closeDownloadOptions()
+    } else {
+        showDownloadOptions()
+    }
+}
+
+document.addEventListener('mouseup', (e) => {
+    const downloadButton = document.getElementsByClassName('download')[0]
+
+    if (!downloadButton.contains(e.target)) {
+        closeDownloadOptions()
+    }
+})
+
+window.download = (file) => {
+    const element = document.createElement('a');
+    document.body.appendChild(element);
+    element.download = 'Pleshakov Andrey CV';
+    element.href = `assets/${file}`;
+    element.click();
+    document.body.removeChild(element);
+}
